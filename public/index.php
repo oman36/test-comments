932405c7aeb5f5a41546fed876bff836bbb67cae
Vladimir <neoman36@gmail.com>
2017-05-01 18:55:14 +0400

    try catch all app

--- public/index.php
+++ public/index.php
@@ -1,15 +1,21 @@
 <?php
-define("ROOT",__DIR__ . "/..");
-require ROOT . "/App/bootstrap.php";
-ini_set("display_errors",config("app.display_errors"));
-\Helpers\View::setPatternsFolder(ROOT . "/App/Views");
-session_start();
-
 try {
+
+    define("ROOT",__DIR__ . "/..");
+    require ROOT . "/App/bootstrap.php";
+    ini_set("display_errors",config("app.display_errors"));
+    \Helpers\View::setPatternsFolder(ROOT . "/App/Views");
+    session_start();
+
     require ROOT . "/App/route.php";
+
 } catch (\Exception $e) {
     if (env("DEBUG")) {
+        http_response_code(500);
         echo "<pre>";
+        echo "<b>" . $e->getMessage(),"<b>\n";
         throw $e;
+    } else {
+        http_response_code(500);
     }
 }
